b28adc8033116850b942a251f7b91a71ea811ff5
Sparsh2509 <sparshgupta250904@gmail.com>
2026-03-03 13:50:44 +0530

    API key error solved

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
+import os
+from dotenv import load_dotenv
 from rag.retriever import retrieve_context
 from prompts.email_prompt import build_email_prompt
 from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash-lite",
