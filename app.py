8887954e9c6921792b48d8c8d5ab634d80d990ad
Sparsh2509 <sparshgupta250904@gmail.com>
2026-03-03 17:12:58 +0530

    Added dynamic input from the user

--- app.py
+++ app.py
@@ -4,27 +4,55 @@ from rag.retriever import retrieve_context
 from prompts.email_prompt import build_email_prompt
 from langchain_google_genai import ChatGoogleGenerativeAI
 
+# Load environment variables
 load_dotenv()
+
+# Load Gemini API Key
 gemini_key = os.getenv("GEMINI_API_KEY")
+
+if not gemini_key:
+    raise ValueError("GEMINI_API_KEY not found in .env file")
+
+# Initialize Gemini LLM
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash-lite",
     temperature=0.7,
     api_key=gemini_key
 )
 
-# Example input
-sender = "Sparsh"
-recipient = "HR Manager"
-company = "AI Startup"
-purpose = "internship request"
-tone = "professional"
-key_points = "- Built RAG systems\n- Strong in Python\n- Passionate about AI"
-length = "medium"
 
 
-query = f"{purpose} {tone} email"
+print("\n===== AI Email Copilot (RAG Powered) =====\n")
 
+sender = input("Enter sender name: ")
+recipient = input("Enter recipient name: ")
+company = input("Enter company name: ")
+purpose = input("Enter purpose (e.g., internship request / cold email / follow up): ")
+tone = input("Enter tone (professional / friendly / persuasive): ")
+length = input("Enter length (short / medium / long): ")
+
+print("\nEnter key points (type 'done' when finished):")
+
+points = []
+while True:
+    line = input()
+    if line.lower() == "done":
+        break
+    points.append("- " + line)
+
+key_points = "\n".join(points)
+
+
+
+query = f"{purpose} {tone} email template"
 context = retrieve_context(query)
+
+
+print("\n----- RETRIEVED CONTEXT FROM FAISS -----\n")
+print(context)
+print("\n-------------------------------------------\n")
+
+
 
 prompt = build_email_prompt(
     sender,
@@ -37,6 +65,10 @@ prompt = build_email_prompt(
     context
 )
 
+
+
 response = llm.invoke(prompt)
 
+print("\n===== GENERATED EMAIL =====\n")
 print(response.content)
+print("\n================================\n")
